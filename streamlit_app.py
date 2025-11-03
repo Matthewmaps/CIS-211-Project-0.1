fee6e5d97ff7545b3b6e541fde80ec3b6e2fcc68
Matt Matt <84437934+Matthewmaps@users.noreply.github.com>
2025-11-03 10:25:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,13 @@ st.set_page_config(
 
 # Custom CSS (optional - for styling)
 st.markdown('''
-  <style>
-  .main-header{font-size: 42px; font-weight: bold; text-align:center;}
-  .sub-header {font_size: 24px; text-align:center; color: #666;}
+    <style>
+        .main-header{font-size: 42px; font-weight: bold; text-align:center;}
+        .sub-header {font_size: 24px; text-align:center; color: #666;}
+      </style>
+      ''', unsafe_allow_html = True)
+
+# Sidebar
+st.sidebar.title('📍Navigation')
+page = st.sidebar.radio('Go to',
+                        ['🏠 Home', '👔 About', '💼 projects', '⚒️ Skills', '📝 Resume', '📩 Contact' ])
